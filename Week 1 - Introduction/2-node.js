76fde2594eef9d6ac43c17702ab2ea68aadaac31
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-03-18 12:10:36 +0100

    week1

---
+++ Week 1 - Introduction/2-node.js
@@ -0,0 +1,23 @@
+/*
+
+var http = require('http');
+
+var server = http.createServer(function(req, resp) {
+	resp.writeHead(200,{'Content-Type': 'text/plain'});
+	resp.end("Hello World\n");
+});
+
+server.listen(8000);
+console.log('Server is running');
+*/
+
+
+
+/*
+*/
+/*
+*/
+/*
+*/
+/*
+*/
