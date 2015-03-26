9ec3e70af652a58232004f4dabfad9974a087183
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-03-26 23:42:39 +0100

    week2

--- Week 2 - CRUD/1 - bash.js
+++ Week 2 - CRUD/1 - bash.js
@@ -3,6 +3,11 @@ db // shows current db name
 db.people.insert(doc) // insert document into people collection of current db
 
 db.people.find() // query, show all
+
+// FIND ONE
 db.people.findOne() // show one
 db.people.findOne({"name":"Ivan"}) // find specific
 db.people.findOne({"name":"Ivan"}, {"name":true, "_id":false}) // show 'name' firld, but hide '_id' field
+
+// FIND
+db.people.find().pretty() // display json
