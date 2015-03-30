d55b4fee137ad978cd161cbac21f5eea550c09ba
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-03-30 23:30:01 +0200

    week2

--- Week 2 - CRUD/hw 2.2.js
+++ Week 2 - CRUD/hw 2.2.js
@@ -1,38 +1,47 @@
-var MongoClient = require('mongodb').MongoClient;
+//  db.data.find().sort({'State':1,'Temperature':-1}).limit(5).pretty()
+/*
+{ State: 'California', Temperature: 81 }
+{ State: 'Florida', Temperature: 83 }
+{ State: 'New Mexico', Temperature: 57 }
+{ State: 'Vermont', Temperature: 57 }
+*/
+var MongoClient = require('mongodb').MongoClient,
+queries=[];
 
 MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
     if(err) throw err;
 
     var grades = db.collection('data');
 
-    var options = {'sort' : [['State', 1], ['Temperature', -1]] };
-    var cursor = grades.find({}, {}, options);
     var state = '';
-    var queries = [];
-    cursor.each(function(err, doc) {
-        if(err) throw err;
-        if(doc == null) {
-            // UPDATE HERE
-            queries.forEach(function(query) {
+    var cursor = grades.find({}).sort({'State':1,'Temperature':-1});
+    cursor.toArray(function(err, doc){
+        doc.forEach(function(el){
+            if(el["State"] !== state){
+            // YES
+                state = el["State"];
+                //console.dir(el["State"]+': '+el["Temperature"]);
+                // FOUND el
+                queries.push({
+                    "State": el["State"],
+                    "Temperature": el["Temperature"]
+                });
+            }
+        });
+
+        queries.forEach(function(query) {
                 console.log(query);
-                db.collection('data').find(query, function(err, doc){
-                    console.log(doc);
+                var operator = {$set: {'month_high': true}};
+                db.collection('data').update(query, operator, function(err, updated) {
+                    console.log('updated');
+                    return db.close();
+                });
+
+                db.collection('data').find(query).toArray(function(err, doc){
+                    if(err) return db.close();
+                    // console.log(doc);
                 });
             });
-
-            return db.close();
-        }
-        // is it unique state
-        if(doc["State"] !== state){
-            // YES
-            state = doc["State"];
-            console.dir(doc["State"]+': '+doc["Temperature"]);
-            // FOUND DOC
-            queries.push({
-                "State": doc["State"],
-                "Temperature": doc["Temperature"]
-            });
-        }
     });
 });
 
