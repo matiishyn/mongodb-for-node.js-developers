b0903f90c2805f0b23c8fa6763da28a7152320c2
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-04-09 17:32:17 +0200

    week4

--- WEEK 4 - PERFORMANCE/1 - bash.js
+++ WEEK 4 - PERFORMANCE/1 - bash.js
@@ -4,3 +4,11 @@ mkdir WT
 mongod -dbpath WT -storageEngine wiredTiger
 mongo // use WT
     db.foo.stats(); // info
+
+// Indexes
+db.students.explain().find({student_id: 5});
+db.students.explain(true).find({student_id: 5});
+
+// add index
+db.students.createIndex({student_id:1}); // asc
+db.students.createIndex({student_id:1, class_id:-1}); // asc
