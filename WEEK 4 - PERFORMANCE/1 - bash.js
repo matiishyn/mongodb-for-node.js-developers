6df9cddcc3815aab09ee800fedf85c6ecdc08ae4
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2015-04-09 17:46:45 +0200

    week4

--- WEEK 4 - PERFORMANCE/1 - bash.js
+++ WEEK 4 - PERFORMANCE/1 - bash.js
@@ -12,3 +12,9 @@ db.students.explain(true).find({student_id: 5});
 // add index
 db.students.createIndex({student_id:1}); // asc
 db.students.createIndex({student_id:1, class_id:-1}); // asc
+
+// read Indexes
+db.students.getIndexes(); // will show array of indexes
+db.students.dropIndex({student_id:1}); // remove index
+
+// Multikey Indexes
